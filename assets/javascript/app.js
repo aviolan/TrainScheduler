b671490cd4733f5b0525c0b2ac61f5ef5092f4ba
Andrew Violan <andrew.violan@gmail.com>
2018-10-28 14:01:30 -0700

    initial site files

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+//Connects Javascript to my Firebase
+var config = {
+    apiKey: "AIzaSyD3846RzsIKbFJ7nu-pNG8zm9KfyJAtPIQ",
+    authDomain: "train-scheduler-d0a15.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d0a15.firebaseio.com",
+    projectId: "train-scheduler-d0a15",
+    storageBucket: "train-scheduler-d0a15.appspot.com",
+    messagingSenderId: "325584575650"
+  };
+  firebase.initializeApp(config);
+
