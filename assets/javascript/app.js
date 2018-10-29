bd94adac503132538aa9c1103de905aa9e9e7574
Andrew Violan <andrew.violan@gmail.com>
2018-10-28 18:08:30 -0700

    initial site files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,12 +18,31 @@ $("#add-train-btn").on("click", function(event) {
   //Grabs user input
   var trainName = $("#input-train").val().trim();
   var trainDest = $("#input-destination").val().trim();
-  var trainTime = moment($("#input-time").val().trim().format("h:mm a");
+  var trainTime = moment($("#input-time").val().trim(), "h:mm a").format('LT');
   var trainFreq = $("#input-frequency").val().trim();
 
+  // Creates local "temporary" object for holding train data
   var newTrain = {
     name: trainName,
     destination: trainDest,
     time: trainTime,
     frequency: trainFreq
   };
+
+  //Uploads train data to database
+  database.ref().push(newTrain);
+
+  //Console logs
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.time);
+  console.log(newTrain.frequency);
+
+  alert("Train successfully added");
+
+  //Clears all of the text-boxes
+  $("input-train").val("");
+  $("input-destination").val("");
+  $("input-time").val("");
+  $("input-frequency").val("");
+});
