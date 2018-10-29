f227007cf615e179ef54aba8de2ea1cf656dc483
Andrew Violan <andrew.violan@gmail.com>
2018-10-28 19:12:15 -0700

    initial site files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,7 @@ $("#add-train-btn").on("click", function(event) {
   //Grabs user input
   var trainName = $("#input-train").val().trim();
   var trainDest = $("#input-destination").val().trim();
-  var trainTime = moment($("#input-time").val().trim(), "h:mm a").format('LT');
+  var trainTime = moment($("#input-time").val().trim(), "h:mm a");
   var trainFreq = $("#input-frequency").val().trim();
 
   // Creates local "temporary" object for holding train data
@@ -45,4 +45,36 @@ $("#add-train-btn").on("click", function(event) {
   $("input-destination").val("");
   $("input-time").val("");
   $("input-frequency").val("");
+});
+
+//Creates firebase event for adding trains to the database and to the html
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+
+    //Store everything into a variable
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().destination;
+    var trainTime = childSnapshot.val().time;
+    var trainFreq = childSnapshot.val().frequency;
+
+    //Train info
+    console.log(trainName);
+    console.log(trainDest);
+    console.log(trainTime);
+    console.log(trainFreq);
+
+    //Calculate the train times
+    var trainNext = moment().endOf(trainTime).fromNow();
+    console.log("ARRIVAL TIME: " + moment().endOf(trainTime).fromNow());
+
+    //Creates new rows
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDest),
+        $("<td>").text(trainFreq),
+        $("<td>").text(trainNext)
+    );
+
+    //Append the new row to the table
+    $("#train-table > tbody").append(newRow);
 });
